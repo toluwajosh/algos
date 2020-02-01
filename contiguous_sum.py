c8c5d20875e9acc26ce65e2df0d4aaf231328375
toluwajosh <tjosh.owoyemi@gmai.com>
2020-02-02 04:56:33 +0900

    remvoed vscode and updated files

--- contiguous_sum.py
+++ contiguous_sum.py
@@ -4,3 +4,9 @@ given a list of integers (both positive and negative)
 returns the sum of the contiguous subsequence with maximum sum.
 Thus, given the sequence (1, 2, -4, 1, 3, -2, 3, -1) it should return 5.
 """
+
+def contiguous_sum(array):
+    pass
+
+if __name__ == "__main__":
+    pass
--- set_check.py
+++ set_check.py
@@ -13,7 +13,6 @@ def set_intersection(list1, list2):
 
 # implement manually
 def set_union_manual(list1, list2):
-    # if len(list1) > len(list2):
     list_dict = {x: x for x in list1}
     new_list = list1
     for ele in list2:
@@ -22,10 +21,21 @@ def set_union_manual(list1, list2):
         new_list.append(ele)
     return new_list
 
+
+def set_intersection_manual(list1, list2):
+    list_dict = {x:x for x in list1}
+    new_list  = []
+    for ele in list2:
+        if ele in list_dict:
+            new_list.append(ele)
+    return new_list
+
 if __name__ == "__main__":
     a = [1, 2, 3, 4, 5, 6, 7]
     b = [6, 7, 8, 9, 0]
-    print(set_union(a, b))
-    # print(set_intersection(a, b))
-    print(set_union_manual(a, b))
+    # print(set_union(a, b))
+    # print(set_union_manual(a, b))
+    
+    print(set_intersection(a, b))
+    print(set_intersection_manual(a, b))
 
